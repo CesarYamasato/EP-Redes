3a9aa494efcff461cbe8685fc74a077fba170bf5
Abreu <abreu@disroot.org>
2022-10-26 13:41:47 -0300

    Client updated.

--- Client/Client.java
+++ Client/Client.java
@@ -1,74 +1,86 @@
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.WindowConstants;
 import javax.swing.border.EmptyBorder;
 
 import java.awt.Component;
 import java.awt.Font;
+import java.awt.event.ActionListener;
 import java.io.File;
 
 public class Label {
-    public Label (String font, int class, int size) {
-        title.setFont(new Font("Sans", Font.BOLD, 24));
-        title.setBorder(new EmptyBorder(21, 0, 10, 0));
-        title.setAlignmentX(Component.CENTER_ALIGNMENT);
+    private JLabel label;
+
+    public Label(String text, String font, int type, int size) {
+        label = new JLabel(text);
+        label.setFont(new Font(font, type, size));
+        label.setBorder(new EmptyBorder(size / 5 * 4, 0, size / 2, 0));
+        label.setAlignmentX(Component.CENTER_ALIGNMENT);
+    }
+
+    public JLabel get() {
+        return label;
     }
 }
 
-    public class Button {
-        private JButton button;
+public class Button {
+    private JButton button;
 
-        public Button(String label) {
-            button = new JButton(label);
-            button.setPreferedSize(new Dimension(150, 48));
-            button.setFont(new Font("Sans", Font.PLAIN, 24));
-        }
-
-        public JButton get() {
-            return button;
-        }
+    public Button(String label) {
+        button = new JButton(label);
+        button.setPreferedSize(new Dimension(150, 48));
+        button.setFont(new Font("Sans", Font.PLAIN, 24));
     }
+
+    public JButton get() {
+        return button;
+    }
+}
 
 public class Client {
     public static void main(String[] args) {
-        File f;
+        final File[] f = new File[1];
         JFrame window = new JFrame("White Rabbit Client");
 
         // Define window properties
 
         window.setSize(450, 450);
         window.setLayout(new BoxLayout(window.getContentPane(), BoxLayout.Y_AXIS));
-        window.setDefaultCloseOperation(window.EXIT_ON_CLOSE);
+        window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
         // Title placement
-        JLabel title = new JLabel("White Rabbit");
-        title.setFont(new Font("Sans", Font.BOLD, 24));
-        title.setBorder(new EmptyBorder(21, 0, 10, 0));
-        title.setAlignmentX(Component.CENTER_ALIGNMENT);
-
-        // File description placement
-        JLabel filename = new JLabel("Choose a file to send");
-        filename.setFont(new Font("Sans", Font.PLAIN, 12));
-        filename.setBorder(new EmptyBorder(50, 0, 0, 0));
-        filename.setAlignmentX(Component.CENTER_ALIGNMENT);
+        Label title = new Label("White Rabbit", "Sans", FONT.BOLD, 24);
+        Label text = new Label("Choose a file to send", "Sans", Font.PLAIN, 12);
 
         // Button placement
-        JPanel button = new JPanel();
-        button.setBorder(new EmptyBorder(21, 0, 10, 0));
+        JPanel buttons = new JPanel();
+        buttons.setBorder(new EmptyBorder(21, 0, 10, 0));
 
-        JButton choose = new Button("Choose file");
-        send.setPreferredSize(new Dimension(150, 48));
-        send.setFont(new Font("Sans", Font.PLAIN, 24));
+        Button choose = new Button("Choose file");
+        Button send = new Button("Send file");
+        Button cancel = new Button("Cancel");
 
-        JButton send = new Button("Send file");
-        send.setPreferredSize(new Dimension(150, 48));
-        send.setFont(new Font("Sans", Font.PLAIN, 24));
+        buttons.add(choose);
 
-        JButton send = new Button("Cancel");
-        send.setPreferredSize(new Dimension(150, 48));
-        send.setFont(new Font("Sans", Font.PLAIN, 24));
+        choose.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser menu = new JFileChooser();
+                menu.setDialogTitle("Choose a file to send");
 
+                if (menu.showOpenDialog(window) == JFileChooser.APPROVE_OPTION) {
+                    f[0] = menu.getSelectedFile();
+                    text.get().setText(d[0].getName());
+                }
+            }
+        });
+
+        send.addActionListener(new ActionListener() {
+            @Override
+        });
     }
 }
--- Peer.java
+++ Peer.java
@@ -3,96 +3,95 @@ import java.net.*;
 import FileManager.*;
 
 public class Peer {
-	Socket clientSocket;
-	ServerSocket serverSocket;
-	
-	public Peer(){
-		try {
-			serverSocket = new ServerSocket(25565);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
-	//Makes the Peer wait for connection from another peer on its serverSocket
-	public void createServer(){
-		try {
-			clientSocket = serverSocket.accept();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-	}
-	
-	//Connects to another peer's serverSocket
-	public void connect(){
-		try {
-			clientSocket = new Socket("localhost", 25565);
-		} catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-	}
-	
-	//Sends the List of items in the folder on which the .class file is on
-	public void sendListDirectory() throws IOException {
-		DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream());
-		
-		File Directory = new File(System.getProperty("user.dir"));
-		String contents[] = Directory.list();
-		
-		out.writeInt(contents.length);
-		
-	      for(int i=0; i<contents.length; i++) {
-	    	  out.writeInt(contents[i].length());
-	    	  out.writeBytes(contents[i]);
-	      }
-	}
-	
-	//Receives the List of items the other peer can send over
-	public void receiveListDirectory() throws IOException {
-		DataInputStream in = new DataInputStream(clientSocket.getInputStream());
-		System.out.println("List of files and directories that can be sent:");
-		int numOfFiles = in.readInt();
-		for(int i=0; i<numOfFiles; i++) {
-			int size = in.readInt();
-			System.out.println(new String(in.readNBytes(size)));
-		}
-	}
-	
-	//Closes the connection with the other peer
-	public void close() {
-		try {
-			this.serverSocket.close();
-			this.clientSocket.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
-	public static void main(String[] args) throws IOException {
-			Peer peer = new Peer();
-			if(args[0].equals("-s")) {
-				peer.createServer();
-				peer.sendListDirectory();
-				File file = new File("teste.txt");
-				FileSender fileSender = new FileSender(peer.clientSocket);
-				fileSender.sendFile(file);
-			}
-			else if(args[0].equals("-c")) {
-				peer.connect();
-				System.out.println("HELLO");
-				peer.receiveListDirectory();
-				FileReceiver fileReceiver = new FileReceiver(peer.clientSocket);
-				fileReceiver.receiveFile();
-			}
-			peer.close();
-	}
+    Socket clientSocket;
+    ServerSocket serverSocket;
+
+    public Peer() {
+        try {
+            serverSocket = new ServerSocket(25565);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    // Makes the Peer wait for connection from another peer on its serverSocket
+    public void createServer() {
+        try {
+            clientSocket = serverSocket.accept();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+
+    // Connects to another peer's serverSocket
+    public void connect() {
+        try {
+            clientSocket = new Socket("191.19.4.238", 25565);
+        } catch (UnknownHostException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+
+    // Sends the List of items in the folder on which the .class file is on
+    public void sendListDirectory() throws IOException {
+        DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream());
+
+        File Directory = new File(System.getProperty("user.dir"));
+        String contents[] = Directory.list();
+
+        out.writeInt(contents.length);
+
+        for (int i = 0; i < contents.length; i++) {
+            out.writeInt(contents[i].length());
+            out.writeBytes(contents[i]);
+        }
+    }
+
+    // Receives the List of items the other peer can send over
+    public void receiveListDirectory() throws IOException {
+        DataInputStream in = new DataInputStream(clientSocket.getInputStream());
+        System.out.println("List of files and directories that can be sent:");
+        int numOfFiles = in.readInt();
+        for (int i = 0; i < numOfFiles; i++) {
+            int size = in.readInt();
+            System.out.println(new String(in.readNBytes(size)));
+        }
+    }
+
+    // Closes the connection with the other peer
+    public void close() {
+        try {
+            this.serverSocket.close();
+            this.clientSocket.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        Peer peer = new Peer();
+        if (args[0].equals("-s")) {
+            peer.createServer();
+            peer.sendListDirectory();
+            File file = new File("teste.txt");
+            FileSender fileSender = new FileSender(peer.clientSocket);
+            fileSender.sendFile(file);
+        } else if (args[0].equals("-c")) {
+            peer.connect();
+            System.out.println("HELLO");
+            peer.receiveListDirectory();
+            FileReceiver fileReceiver = new FileReceiver(peer.clientSocket);
+            fileReceiver.receiveFile();
+        }
+        peer.close();
+    }
 }
