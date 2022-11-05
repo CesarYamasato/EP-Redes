156971daa7d651d9d303dab1b616c5cbca0d4f98
Abreu <abreu@disroot.org>
2022-11-05 20:31:46 -0300

    Keep em separated

--- cli/main/Peer.java
+++ cli/main/Peer.java
@@ -1,3 +1,4 @@
+package main;
 
 import java.io.*;
 import java.net.*;
@@ -9,11 +10,12 @@ public class Peer extends Thread {
     private Server server;
     private Client client;
 
-    // ====================================== Server Side =============================================================//
+    // ====================================== Server Side
+    // =============================================================//
 
     private String clientAddress;
     private int port;
-    
+
     public void createServer(int port) throws UnknownHostException, IOException {
         ServerSocket serverSocket = new ServerSocket(port);
         Socket clientSocket = serverSocket.accept();
@@ -32,15 +34,17 @@ public class Peer extends Thread {
     }
 
     public String getAddress() {
-    	return clientAddress;
+        return clientAddress;
     }
+
     public int getPort() {
-    	return port;
+        return port;
     }
-    
+
     public void run() {
         try {
-            while (true && !this.isClosed()) receiveRequest();
+            while (true && !this.isClosed())
+                receiveRequest();
             this.close();
             Thread.yield();
         } catch (IOException e) {
@@ -49,7 +53,8 @@ public class Peer extends Thread {
         }
     }
 
-    // ====================================== Client Side ============================================================//
+    // ====================================== Client Side
+    // ============================================================//
 
     public void connect(String IP, int port) throws UnknownHostException, IOException {
         Socket clientSocket = new Socket(IP, port);
@@ -59,8 +64,10 @@ public class Peer extends Thread {
     }
 
     public void sendRequest() throws IOException {
-        if(!this.isClosed())client.sendRequest();
-        else client.close();
+        if (!this.isClosed())
+            client.sendRequest();
+        else
+            client.close();
     }
 
     public void sendAwake(int port) throws IOException {
@@ -68,28 +75,32 @@ public class Peer extends Thread {
     }
 
     public boolean isClosed() {
-    	return server.isClosed() || client.isClosed() ? true : false;
+        return server.isClosed() || client.isClosed() ? true : false;
     }
-    
+
     public void close() {
-    	if(!server.isClosed()) server.close();
-    	if(!client.isClosed()) client.close();
-    	System.exit(0);
+        if (!server.isClosed())
+            server.close();
+        if (!client.isClosed())
+            client.close();
+        System.exit(0);
     }
+
     public static void main(String[] args) throws IOException {
         Peer peer = new Peer();
         if (args[0].equals("-s")) {
             peer.createServer(Integer.parseInt(args[1]));
             peer.waitAwake();
-            peer.connect(peer.getAddress(),peer.getPort());
+            peer.connect(peer.getAddress(), peer.getPort());
         } else if (args[0].equals("-c")) {
             peer.connect(args[2], Integer.parseInt(args[3]));
             peer.sendAwake(Integer.parseInt(args[1]));
             peer.createServer(Integer.parseInt(args[1]));
         }
         peer.start();
-        while (true && !peer.isClosed()) peer.sendRequest();
+        while (true && !peer.isClosed())
+            peer.sendRequest();
         peer.close();
-        
+
     }
 }
