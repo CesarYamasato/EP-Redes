021769fe5f7f3e85bb31bd3d5e3556bc47d8cc4d
CesarYamasato <cesar.yamasato@usp.br>
2024-01-30 15:47:05 -0300

    Block of data from block class no longer contains size in it

--- src/socketApplication/Block.java
+++ src/socketApplication/Block.java
@@ -74,27 +74,7 @@ public class Block {
     LinkedList<byte[]> block = new LinkedList<byte[]>();
     int size = 0;
 
-    public static void main(String[] args) throws IOException{
-        Block block = new Block();
-        String teste = "aaabbb";
-        String teste2 = "bbbcc";
-        block.append(teste.getBytes());
-        block.append(teste2.getBytes());
-        block.append(teste.getBytes());
-
-        byte[] to_print = block.getBlock();
-        byte[] size = Arrays.copyOfRange(to_print, 0, Integer.BYTES);
-        byte[] rest = Arrays.copyOfRange(to_print, Integer.BYTES, to_print.length);
-        int sizeInt = ByteBuffer.wrap(size).getInt();
-        int size_of_string = teste.getBytes().length + teste2.getBytes().length + teste.getBytes().length;
-        System.out.println("String size: " + size_of_string);
-        System.out.println(sizeInt);
-        System.out.println("Size of Int: " + Integer.BYTES);
-        System.out.print(new String(rest));
-    }
-
     public byte[] getBlock() {
-        byte[] lenght = ByteBuffer.allocate(Integer.BYTES).putInt(size+Integer.BYTES).array();
         byte[] return_block = new byte[size];
 
         int index = 0;
@@ -104,17 +84,6 @@ public class Block {
                 index++;
             }
         }
-
-        try {
-            ByteArrayOutputStream outputStream = new ByteArrayOutputStream( );
-            outputStream.write( lenght );
-            outputStream.write( return_block );
-
-            return_block = outputStream.toByteArray( );
-        } catch (IOException e) {
-            System.out.println("Unnable to produce block");
-        }
-
         return return_block;
     }
 
